64b92a9348ac66c61738cf2d33747923cb4aa699
NuraliAKH <axmadovnurali5@gmail.com>
2024-07-20 16:18:57 +0500

    added AbortController to cencel

--- src/models/users/users.controller.ts
+++ src/models/users/users.controller.ts
@@ -1,13 +1,40 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Req, Res } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { SearchDto } from './dto/search.dto';
+import { Request, Response } from 'express';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('search')
-  async search(@Query() query: SearchDto) {
-    return this.usersService.findUsers(query.email, query.number);
+  async search(
+    @Query() query: SearchDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const abortController = new AbortController();
+
+    req.on('close', () => {
+      console.log('Соединение закрыто клиентом');
+      abortController.abort();
+    });
+
+    try {
+      const result = await this.usersService.findUsers(
+        query.email,
+        query.number,
+        abortController.signal,
+      );
+      res.json(result);
+    } catch (error) {
+      if (error.name === 'AbortError') {
+        console.log('Request aborted:', error.message);
+        res.status(499).json({ message: 'Request aborted' });
+      } else {
+        console.error('Error during user search:', error);
+        res.status(500).json({ error: error.message });
+      }
+    }
   }
 }
--- src/models/users/users.service.ts
+++ src/models/users/users.service.ts
@@ -11,17 +11,25 @@ export class UsersService {
     this.users = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
   }
 
-  async findUsers(email: string, number?: string) {
-    try {
-      await new Promise((resolve) => setTimeout(resolve, 5000));
-      return this.users.filter(
-        (user) =>
-          user.email.includes(email) &&
-          (!number || user.number.includes(number)),
-      );
-    } catch (error) {
-      console.error('Error during user search:', error);
-      throw error;
-    }
+  async findUsers(
+    email: string,
+    number: string | undefined,
+    signal: AbortSignal,
+  ) {
+    return new Promise((resolve, reject) => {
+      const timeout = setTimeout(() => {
+        const results = this.users.filter(
+          (user) =>
+            user.email.includes(email) &&
+            (!number || user.number.includes(number)),
+        );
+        resolve(results);
+      }, 5000);
+
+      signal.addEventListener('abort', () => {
+        clearTimeout(timeout);
+        reject(new Error('Запрос был отменен'));
+      });
+    });
   }
 }
